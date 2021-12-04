a521cd028cac54a6cbe3664452eb889649b9ce82
Florian Marending <florian@marending.dev>
2021-12-04 22:20:28 +0100

    Add highlight to fieldnotes name

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '/fieldnotes.png';
 import { HappyIcon } from './icons';
+import '../style.css';
 
 const App = (): React.ReactElement => {
   return (
     <div className="relative flex flex-row p-8">
       <div className="absolute flex flex-row gap-4 items-center">
         <img className="w-16 h-16" src={logo} />
-        <div className="text-2xl font-bold">fieldnotes</div>
+        <div className="text-2xl font-bold highlight">fieldnotes</div>
       </div>
       <div className="w-full pt-16 flex flex-col items-center">
         <div className="flex flex-col items-center">
